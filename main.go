d5ffc1dd99f0fdf242fe3d223cb602dd655e03bd
Richard Ochom <ochomrichard752@gmail.com>
2023-09-04 22:41:25 +0300

    update logging

--- main.go
+++ main.go
@@ -48,10 +48,10 @@ func wrapRequest(url string, headers map[string]string, payload any) (*gttp.Resp
 		"payload": payload,
 		"url":     url,
 	}
-	fmt.Printf("register user: %+v\n", printable)
 
 	res, err := gttp.NewRequest(url, headers, payload).Post()
 	if err != nil {
+		fmt.Printf("register user: %+v\n", printable)
 		return nil, fmt.Errorf("http err : %v", err)
 	}
 
